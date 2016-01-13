484609008edebc362de8083fe9a94e743bd1d972
xionghaiping <799652526@qq.com>
2016-01-13 16:17:33 +0800

    .

--- src/com/hai/idmanager/BaseActivity.java
+++ src/com/hai/idmanager/BaseActivity.java
@@ -2,7 +2,7 @@ package com.hai.idmanager;
 
 import android.app.Activity;
 
-public class BaseActivity extends Activity {
+public class BaseActivity extends Activity { 
 	@SuppressWarnings("unchecked")
 	protected <T> T findView(int id){
         return (T) findViewById(id);
