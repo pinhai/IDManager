073fc17c45f3c3e9ee828237e8d0daaa3d5969e0
xionghaiping <799652526@qq.com>
2016-08-17 15:48:13 +0800

    。。。

--- src/com/hai/idmanager/MainActivity.java
+++ src/com/hai/idmanager/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends BaseActivity implements OnClickListener{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main2);
+		setContentView(R.layout.activity_main);
 		
 		initData();
 		initView();
